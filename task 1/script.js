7d7d717a272b71453a8037068860f25afb0d2c5e
antelmari <99552050+antelmari@users.noreply.github.com>
2023-03-26 21:38:53 +0300

    task 1

---
+++ task 1/script.js
@@ -0,0 +1,36 @@
+function updateTime() {
+    const hours = document.querySelector('#hours');
+    const minutes = document.querySelector('#minutes');
+    const seconds = document.querySelector('#seconds');
+
+    let time = Number(hours.value) * 60 * 60 + Number(minutes.value) * 60 + Number(seconds.value);
+    if (time <= 0) {
+        clearInterval(timer);
+    } else {
+        --time;
+        console.log(time);
+        hours.value = Math.floor(time / (60 * 60));
+        minutes.value = Math.floor(time / 60);
+        seconds.value = time % 60;
+    }
+}
+
+// запуск и остановка таймера по клику на соответствующую кнопку
+const start = document.querySelector('.start');
+const stop = document.querySelector('.stop');
+let timer = false;
+start.addEventListener('click', () => {
+    if (!timer) {
+        timer = setInterval(updateTime, 1000);
+    }
+});
+stop.addEventListener('click', () => {
+    clearInterval(timer);
+    timer = false;
+});
+
+// разрешение вводить в инпуты только цифры
+function numbers() {
+    if (event.keyCode < 48 || event.keyCode > 57)
+    event.preventDefault();
+}
